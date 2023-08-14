b664de35daa6bc9629f1975596af1b6aeb3bc678
Adem Kolenovic <ademkolenovic@gmail.com>
2023-08-13 22:25:36 -0400

    a

---
+++ part1/courseinfo/src/Content.js
@@ -0,0 +1,13 @@
+import Part from './Part';
+
+const Content = (props) => {
+    const parts = props.parts.map((part, index) => <Part key={index} part={part.name} exercise={part.exercises} />)
+
+    return (
+        <div>
+            {parts}
+        </div>
+    )
+}
+
+export default Content;
